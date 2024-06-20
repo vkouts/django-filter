5b6c86356719c4b99ae0065eb59e7c6558097339
RasaSporT <63489341+rasasport@users.noreply.github.com>
2024-06-21 00:17:38 +0300

    Fix

--- django_filters/rest_framework/filterset.py
+++ django_filters/rest_framework/filterset.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 
 from django import forms
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from django_filters import filterset
 
